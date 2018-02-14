da02ecfc0899d9b8eafe80813e9601873205f449
Andrew Trahan <atrahan19@gmail.com>
2018-02-14 00:02:53 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,8 @@
+function theBeatlesPlay(musicians, instruments){
+  var playsArray = [];
+  for (var i = 0; i < musicians.length; i++){
+    playArray.append(musicians[0] + ' plays ' + instruments[0]);
+  }
+return playsArray
+}
+
